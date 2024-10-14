8b9ef0579579be893a0f0a01e88e684810922669
martinezpke <martinezpke26@gmail.com>
2024-10-13 19:53:50 -0500

    corregir

--- src/utils/project.js
+++ src/utils/project.js
@@ -1,25 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
+import { url } from 'inspector';
 
-export const createProjectStructure = (projectDir, projectType) => {
-  const srcDir = path.join(projectDir, 'src');
-  fs.mkdirSync(srcDir, { recursive: true });
-
-  if (projectType === 'api') {
-    console.log(chalk.blue('  Creando estructura para la API...'));
-    fs.mkdirSync(path.join(srcDir, 'controllers'), { recursive: true });
-    fs.mkdirSync(path.join(srcDir, 'models'), { recursive: true });
-    fs.mkdirSync(path.join(srcDir, 'routes'), { recursive: true });
-    fs.mkdirSync(path.join(srcDir, 'middlewares'), { recursive: true });
-    fs.writeFileSync(path.join(srcDir, 'index.js'), '// Código inicial para la API');
-    console.log(chalk.green('  Estructura para la API creada con éxito!'));
-  } else {
-    console.log(chalk.blue('  Creando estructura para la Aplicación Monolítica...'));
-    fs.mkdirSync(path.join(srcDir, 'components'), { recursive: true });
-    fs.mkdirSync(path.join(srcDir, 'routes'), { recursive: true });
-    fs.mkdirSync(path.join(srcDir, 'services'), { recursive: true });
-    fs.writeFileSync(path.join(srcDir, 'app.js'), '// Código inicial para la aplicación monolítica');
-    console.log(chalk.green('  Estructura para la Aplicación Monolítica creada con éxito!'));
+export const createProjectStructure = (git, answers) => {
+  const repoUrl = "";
+  if (answers.projectType === "api" ) {
+    repoUrl = "https://github.com/martinezpke/API-FILE-STRUCTURE.git"
+  } else if (answers.projectType === "monolith") {
+    repoUrl = "https://github.com/martinezpke/APP-MONOLITICA-FILE-STRUCTURE.git"
+  } else if (answers.projectType === "cli") {
+    repoUrl = "https://github.com/martinezpke/CLI-APP-FILE-STRUCTURE.git"
   }
 };
+
+
